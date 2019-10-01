768daade59054e2eeb23a5145a5fcfa3e587c5d9
Hongjiseong <31815902+Hongjiseong@users.noreply.github.com>
2019-10-01 15:53:13 +0900

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// src/js/main.js
+import "@babel/polyfill";
+import { pi, power, Foo } from './lib';
+
+console.log(pi);
+console.log(power(pi, pi));
+
+const f = new Foo();
+console.log(f.foo());
+console.log(f.bar());
